28be5ef74ce35aefa46f700785206e9ff86d1ad5
BarryGeeMOJ <104000682+BarryGeeMOJ@users.noreply.github.com>
2023-04-20 15:20:54 +0100

    Rename template project and configure slack channels (#1)

---
+++ logger.ts
@@ -0,0 +1,9 @@
+import bunyan from 'bunyan'
+import bunyanFormat from 'bunyan-format'
+import config from './server/config'
+
+const formatOut = bunyanFormat({ outputMode: 'short', color: !config.production })
+
+const logger = bunyan.createLogger({ name: 'Hmpps Launchpad', stream: formatOut, level: 'debug' })
+
+export default logger
