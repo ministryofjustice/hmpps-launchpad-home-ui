ae5d65acdd7d71f477b22ef70a6827c427f8c885
Barry Gee <barry.gee@digital.justice.gov.uk>
2023-04-25 12:22:34 +0100

    Update application name title

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -10,7 +10,7 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'Hmpps Launchpad'
+  app.locals.applicationName = 'HMPPS Launchpad'
 
   // Cachebusting version string
   if (production) {
