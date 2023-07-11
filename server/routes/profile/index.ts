45d089fb8cea17d36217fbe8b3aba2923a073bdf
Barry Gee <barry.gee@digital.justice.gov.uk>
2023-07-11 14:42:02 +0100

    remove unused post

--- server/routes/profile/index.ts
+++ server/routes/profile/index.ts
@@ -9,7 +9,6 @@ import ProfileController from './profileController'
 export default function routes(services: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
-  const post = (path: string | string[], handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
 
   const profile = new ProfileController(services)
 
