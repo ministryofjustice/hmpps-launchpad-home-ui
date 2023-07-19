ecf6bff05ae983a34206813830b36b6d3cd9e0d7
Barry Gee <barry.gee@digital.justice.gov.uk>
2023-07-19 12:06:35 +0100

    Add unit test for formatDate()

--- server/utils/utils.test.ts
+++ server/utils/utils.test.ts
@@ -1,4 +1,4 @@
-import { convertToTitleCase, initialiseName } from './utils'
+import { convertToTitleCase, initialiseName, formatDate } from './utils'
 
 describe('convert to title case', () => {
   it.each([
@@ -26,5 +26,50 @@ describe('initialise name', () => {
     ['Double barrelled', 'Robert-John Smith-Jones-Wilson', 'R. Smith-Jones-Wilson'],
   ])('%s initialiseName(%s, %s)', (_: string, a: string, expected: string) => {
     expect(initialiseName(a)).toEqual(expected)
+  })
+})
+
+describe('convert to title case', () => {
+  it.each([
+    [null, null, ''],
+    ['empty string', '', ''],
+    ['Lower case', 'robert', 'Robert'],
+    ['Upper case', 'ROBERT', 'Robert'],
+    ['Mixed case', 'RoBErT', 'Robert'],
+    ['Multiple words', 'RobeRT SMiTH', 'Robert Smith'],
+    ['Leading spaces', '  RobeRT', '  Robert'],
+    ['Trailing spaces', 'RobeRT  ', 'Robert  '],
+    ['Hyphenated', 'Robert-John SmiTH-jONes-WILSON', 'Robert-John Smith-Jones-Wilson'],
+  ])('%s convertToTitleCase(%s, %s)', (_: string, a: string, expected: string) => {
+    expect(convertToTitleCase(a)).toEqual(expected)
+  })
+})
+
+describe('initialise name', () => {
+  it.each([
+    [null, null, null],
+    ['Empty string', '', null],
+    ['One word', 'robert', 'r. robert'],
+    ['Two words', 'Robert James', 'R. James'],
+    ['Three words', 'Robert James Smith', 'R. Smith'],
+    ['Double barrelled', 'Robert-John Smith-Jones-Wilson', 'R. Smith-Jones-Wilson'],
+  ])('%s initialiseName(%s, %s)', (_: string, a: string, expected: string) => {
+    expect(initialiseName(a)).toEqual(expected)
+  })
+})
+
+describe('format date', () => {
+  let date: Date
+
+  beforeEach(() => {
+    date = new Date('2025-12-01T10:30:00')
+  })
+
+  afterEach(() => {
+    date = null
+  })
+
+  it('it should return a string formatted version of the provided Date object', () => {
+    expect(formatDate(date)).toEqual('Monday 1 December, 2025')
   })
 })
