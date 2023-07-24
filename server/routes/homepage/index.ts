222287fac171d79ad381b26bfec5041fb7f34566
Barry Gee <barry.gee@digital.justice.gov.uk>
2023-07-24 11:41:40 +0100

    Refactor routers

--- server/routes/homepage/index.ts
+++ server/routes/homepage/index.ts
@@ -3,17 +3,78 @@ import { type RequestHandler, Router } from 'express'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 
 import type { Services } from '../../services'
-
-import HomepageController from './homepageController'
+import { formatDate } from '../../utils/utils'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(services: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
-  const homepage = new HomepageController(services)
+  const eventsData = {
+    isTomorrow: false,
+    error: false,
+    events: [
+      {
+        timeString: '8:30am to 11:45am',
+        description: 'Event description',
+        location: 'Event location',
+      },
+      {
+        timeString: '1:45pm to 4:45pm',
+        description: 'Event description',
+        location: 'Event location',
+      },
+      {
+        timeString: '6:30pm to 7:45pm',
+        description: 'Event description',
+        location: 'Event location',
+      },
+    ],
+  }
 
-  get('/', homepage.view())
+  const linksData = {
+    title: '',
+    links: [
+      {
+        image: '/assets/images/link-tile-images/unilink-link-tile-image.png',
+        title: 'Unilink',
+        url: '#',
+        description: 'Access to kiosk apps',
+        openInNewTab: true,
+      },
+      {
+        image: '/assets/images/link-tile-images/content-hub-link-tile-image.png',
+        title: 'Content Hub',
+        url: '#',
+        description: 'Watch, read and listen to local and national content',
+        openInNewTab: false,
+      },
+      {
+        image: '/assets/images/link-tile-images/npr-link-tile-image.png',
+        title: 'NPR',
+        url: '#',
+        description: 'Listen to 24/7 music, talk, requests and playbacks',
+        openInNewTab: true,
+      },
+      {
+        image: '/assets/images/link-tile-images/inside-time-link-tile-image.png',
+        title: 'Inside Time',
+        url: 'https://insidetimeprison.org/',
+        description: 'Read the national newspaper for prisoners and detainees',
+        openInNewTab: true,
+      },
+    ],
+  }
+
+  get('/', (req, res) => {
+    return res.render('pages/homepage', {
+      errors: req.flash('errors'),
+      message: req.flash('message'),
+      today: formatDate(new Date()),
+      eventsData,
+      linksData,
+    })
+  })
 
   return router
 }
--- server/routes/profile/index.ts
+++ server/routes/profile/index.ts
@@ -3,16 +3,17 @@ import { type RequestHandler, Router } from 'express'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import type { Services } from '../../services'
 
-import ProfileController from './profileController'
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(services: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
-  const profile = new ProfileController(services)
-
-  get('/', profile.view())
+  get('/', (req, res) => {
+    return res.render('pages/profile', {
+      errors: req.flash('errors'),
+      message: req.flash('message'),
+    })
+  })
 
   return router
 }
