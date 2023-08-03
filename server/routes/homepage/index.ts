74876933ff21550c51bd68bcb6de646b2ea46175
Barry Gee <barry.gee@digital.justice.gov.uk>
2023-08-03 11:32:01 +0100

    Remove unneeded eslint comment

--- server/routes/homepage/index.ts
+++ server/routes/homepage/index.ts
@@ -5,7 +5,6 @@ import asyncMiddleware from '../../middleware/asyncMiddleware'
 import type { Services } from '../../services'
 import { formatDate } from '../../utils/utils'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function routes(services: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
