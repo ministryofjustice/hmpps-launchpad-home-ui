9e704587b0714b4807da9e5cd51f1c36b36e8ce2
Barry Gee <barry.gee@digital.justice.gov.uk>
2023-10-06 11:39:23 +0100

    Add prison api call for events summary data
    remove comment

--- server/@types/launchpad/index.d.ts
+++ server/@types/launchpad/index.d.ts
@@ -1,3 +1,25 @@
+export type RawPrisonerEvent = {
+  bookingId: number
+  eventClass: string
+  eventId: number
+  eventStatus: string
+  eventType: string
+  eventTypeDesc: string
+  eventSubType: string
+  eventSubTypeDesc: string
+  eventDate: string
+  startTime: string
+  endTime: string
+  eventLocation: string
+  eventLocationId: number
+  eventSource: string
+  eventSourceCode: string
+  eventSourceDesc: string
+  paid: boolean
+  payRate: number
+  locationCode: string
+}
+
 export type PrisonerEvent = {
   timeString: string
   description: string
--- server/data/prisonApiClient.ts
+++ server/data/prisonApiClient.ts
@@ -1,35 +1,49 @@
-import { EventsData, PrisonerEvent } from '../@types/launchpad'
+import { EventsData, PrisonerEvent, RawPrisonerEvent } from '../@types/launchpad'
+import RestClient from './restClient'
+import config, { ApiConfig } from '../config'
 
 export default class PrisonApiClient {
-  token: string
-
-  bookingId: string
+  private restClient: RestClient
 
   constructor(token: string) {
-    this.token = token
+    this.restClient = new RestClient('prisonApiClient', config.apis.prison as ApiConfig, token)
   }
 
   async getEventsSummary(bookingId: string): Promise<EventsData> {
-    this.bookingId = bookingId
+    const rawPrisonerEvents = (await this.restClient.get({
+      path: `/api/bookings/${bookingId}/events/today`,
+      query: new URLSearchParams({ activeRestrictionsOnly: 'true' }).toString(),
+    })) as RawPrisonerEvent[]
+
+    const prisonerEvents: PrisonerEvent[] = []
+
+    rawPrisonerEvents.forEach(rawPrisonerEvent => {
+      const prisonerEvent: PrisonerEvent = {
+        timeString: `${rawPrisonerEvent.startTime} to ${rawPrisonerEvent.endTime}`, // update to format as in content hub - replicate functionality
+        description: `${rawPrisonerEvent.eventSourceDesc}`, // update to format as in content hub - replicate functionality
+        location: `${rawPrisonerEvent.eventLocation}`, // update to format as in content hub - replicate functionality
+      }
+      prisonerEvents.push(prisonerEvent)
+    })
 
     // temp data - THIS WILL BE REPLACED WITH HTTP REQUEST TO THE PRISON API ENDPOINT
-    const prisonerEvents: PrisonerEvent[] = [
-      {
-        timeString: '8:30am to 11:45am',
-        description: 'Event description',
-        location: 'Event location',
-      },
-      {
-        timeString: '1:45pm to 4:45pm',
-        description: 'Event description',
-        location: 'Event location',
-      },
-      {
-        timeString: '6:30pm to 7:45pm',
-        description: 'Event description',
-        location: 'Event location',
-      },
-    ]
+    // const prisonerEvents: PrisonerEvent[] = [
+    //   {
+    //     timeString: '8:30am to 11:45am',
+    //     description: 'Event description',
+    //     location: 'Event location',
+    //   },
+    //   {
+    //     timeString: '1:45pm to 4:45pm',
+    //     description: 'Event description',
+    //     location: 'Event location',
+    //   },
+    //   {
+    //     timeString: '6:30pm to 7:45pm',
+    //     description: 'Event description',
+    //     location: 'Event location',
+    //   },
+    // ]
 
     const eventsData: EventsData = {
       isTomorrow: false,
