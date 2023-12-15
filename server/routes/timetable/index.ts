327db96e1a6fe8202b839a4ea9b416163ebdae1c
BarryGeeMOJ <104000682+BarryGeeMOJ@users.noreply.github.com>
2023-12-15 17:07:41 +0000

    Update timetable router with last-week and next-week (#77)
    * Update timetable router with last-week and next-week
    
    * Remove format import

--- server/routes/timetable/index.ts
+++ server/routes/timetable/index.ts
@@ -1,5 +1,5 @@
 import { type RequestHandler, Router } from 'express'
-import { addDays } from 'date-fns'
+import { addDays, subDays } from 'date-fns'
 
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import type { Services } from '../../services'
@@ -33,12 +33,54 @@ export default function routes(services: Services): Router {
     })
   })
 
-  get('/last-week', (req, res) => {
-    return res.json({ last: 'week' })
+  get('/last-week', async (req, res) => {
+    const config = {
+      content: false,
+      header: false,
+      postscript: true,
+      detailsType: 'small',
+      lastWeek: true,
+      nextWeek: false,
+    }
+
+    const today = new Date()
+    const fromDate = subDays(today, 7)
+    const toDate = subDays(today, 1)
+
+    const events = await Promise.all([services.prisonerProfileService.getEventsFor(res.locals.user, fromDate, toDate)])
+
+    return res.render('pages/timetable', {
+      title: 'Timetable',
+      config,
+      events,
+      errors: req.flash('errors'),
+      message: req.flash('message'),
+    })
   })
 
-  get('/next-week', (req, res) => {
-    return res.json({ next: 'week' })
+  get('/next-week', async (req, res) => {
+    const config = {
+      content: false,
+      header: false,
+      postscript: true,
+      detailsType: 'small',
+      lastWeek: false,
+      nextWeek: true,
+    }
+
+    const today = new Date()
+    const fromDate = addDays(today, 7)
+    const toDate = addDays(fromDate, 6)
+
+    const events = await Promise.all([services.prisonerProfileService.getEventsFor(res.locals.user, fromDate, toDate)])
+
+    return res.render('pages/timetable', {
+      title: 'Timetable',
+      config,
+      events,
+      errors: req.flash('errors'),
+      message: req.flash('message'),
+    })
   })
 
   return router
