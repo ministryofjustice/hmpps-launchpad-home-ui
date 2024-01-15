fee8007b31e66ae7eb147b23b1d8302b6703b0df
Barry Gee <barry.gee@digital.justice.gov.uk>
2024-01-15 11:09:28 +0000

    Add given name

--- server/routes/profile/index.ts
+++ server/routes/profile/index.ts
@@ -9,7 +9,10 @@ export default function routes(services: Services): Router {
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', (req, res) => {
+    const { given_name: givenName } = res.locals.user.idToken
+
     return res.render('pages/profile', {
+      givenName,
       errors: req.flash('errors'),
       message: req.flash('message'),
     })
