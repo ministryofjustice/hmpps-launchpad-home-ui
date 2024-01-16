35e8a96ce0581597d3743e89d66480b21dd39380
Barry Gee <barry.gee@digital.justice.gov.uk>
2024-01-16 07:27:22 +0000

    Add /profile route

--- server/routes/profile/index.ts
+++ server/routes/profile/index.ts
@@ -8,11 +8,20 @@ export default function routes(services: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
-  get('/', (req, res) => {
+  get('/', async (req, res) => {
+    const today = new Date()
+
+    const timetableEvents = await Promise.all([
+      services.prisonerProfileService.getEventsFor(res.locals.user, today, today),
+    ])
+
     const { given_name: givenName } = res.locals.user.idToken
 
     return res.render('pages/profile', {
       givenName,
+      data: {
+        timetableEvents,
+      },
       errors: req.flash('errors'),
       message: req.flash('message'),
     })
