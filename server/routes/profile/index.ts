c6674a2a27338cf2b8a5c0786e2b176186a8ca95
Barry Gee <barry.gee@digital.justice.gov.uk>
2024-01-17 14:34:25 +0000

    Update links

--- server/routes/profile/index.ts
+++ server/routes/profile/index.ts
@@ -1,4 +1,5 @@
 import { type RequestHandler, Router } from 'express'
+import { getEstablishmentLinksData } from '../../utils/utils'
 
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import type { Services } from '../../services'
@@ -12,15 +13,20 @@ export default function routes(services: Services): Router {
     const today = new Date()
 
     const timetableEvents = await Promise.all([
-      services.prisonerProfileService.getEventsFor(res.locals.user, today, today),
+      services.prisonerProfileService.getEventsFor(res.locals.user, today, today), // update to getEventsForToday()
     ])
 
+    const { prisonerContentHubURL } = await getEstablishmentLinksData(res.locals.user.idToken.establishment.agency_id)
+
     const { given_name: givenName } = res.locals.user.idToken
+
+    // console.log(timetableEvents)
 
     return res.render('pages/profile', {
       givenName,
       data: {
         timetableEvents,
+        prisonerContentHubURL: `${prisonerContentHubURL}/tags/1341`,
       },
       errors: req.flash('errors'),
       message: req.flash('message'),
