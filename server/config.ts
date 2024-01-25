318f584f601b2d35353765127c6e0d8cf89c3375
BarryGeeMOJ <104000682+BarryGeeMOJ@users.noreply.github.com>
2024-01-25 07:01:19 +0000

    Update env var (#87)

--- server/config.ts
+++ server/config.ts
@@ -124,7 +124,7 @@ export default {
       agent: new AgentConfig(Number(get('PRISONER_DETAILS_API_TIMEOUT_RESPONSE', 10000))),
     },
     prisonerContactRegistry: {
-      url: get('PRISONER_CONTACT_REGISTRY_BASE_URL', 'http://localhost:8080', requiredInProduction),
+      url: get('PRISONER_CONTACT_REGISTRY_API_URL', 'http://localhost:8080', requiredInProduction),
       timeout: {
         response: Number(get('PRISONER_DETAILS_API_TIMEOUT_RESPONSE', 10000)),
         deadline: Number(get('PRISONER_DETAILS_API_TIMEOUT_DEADLINE', 10000)),
