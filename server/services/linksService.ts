61a264275227c31a6e0219599fed55a5e23e0c78
BarryGeeMOJ <104000682+BarryGeeMOJ@users.noreply.github.com>
2024-02-27 13:28:27 +0000

    Update tile images assets (#99)
    * Update tile images
    
    * Update file extensions

--- server/services/linksService.ts
+++ server/services/linksService.ts
@@ -9,28 +9,28 @@ export default class Linkservice {
 
     const links: Link[] = [
       {
-        image: '/assets/images/link-tile-images/unilink-link-tile-image.png',
+        image: '/assets/images/link-tile-images/unilink-link-tile-image.jpg',
         title: 'Self-service',
         url: selfServiceURL,
         description: 'Access to kiosk apps',
         openInNewTab: true,
       },
       {
-        image: '/assets/images/link-tile-images/content-hub-link-tile-image.png',
+        image: '/assets/images/link-tile-images/content-hub-link-tile-image.jpg',
         title: 'Content Hub',
         url: prisonerContentHubURL,
         description: 'Watch, read and listen to local and national content',
         openInNewTab: true,
       },
       {
-        image: '/assets/images/link-tile-images/npr-link-tile-image.png',
+        image: '/assets/images/link-tile-images/npr-link-tile-image.jpg',
         title: 'NPR',
         url: `${prisonerContentHubURL}/tags/785`,
         description: 'Listen to 24/7 music, talk, requests and playbacks',
         openInNewTab: true,
       },
       {
-        image: '/assets/images/link-tile-images/inside-time-link-tile-image.png',
+        image: '/assets/images/link-tile-images/inside-time-link-tile-image.jpg',
         title: 'Inside Time',
         url: 'https://insidetimeprison.org/',
         description: 'Read the national newspaper for prisoners and detainees',
