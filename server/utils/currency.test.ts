4bb38dd3538161f0bcf9eeca9152b9080d3838c9
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2024-05-16 10:23:13 +0100

    add currency tests

--- server/utils/currency.test.ts
+++ server/utils/currency.test.ts
@@ -1,5 +1,28 @@
 import { formatCurrency } from './currency'
 
-describe(formatCurrency.name, () => {
-  it('should format the currency correctly', () => {})
+describe('formatCurrency function', () => {
+  it('should format currency correctly for GBP', () => {
+    const formattedAmount = formatCurrency(1000, 'GBP')
+    expect(formattedAmount).toBe('£1,000.00')
+  })
+
+  it('should handle null amount', () => {
+    const formattedAmount = formatCurrency(null, 'GBP')
+    expect(formattedAmount).toBeNull()
+  })
+
+  it('should handle NaN amount', () => {
+    const formattedAmount = formatCurrency(NaN, 'GBP')
+    expect(formattedAmount).toBeNull()
+  })
+
+  it('should handle missing currency', () => {
+    const formattedAmount = formatCurrency(1000, '')
+    expect(formattedAmount).toBeNull()
+  })
+
+  it('should handle undefined currency', () => {
+    const formattedAmount = formatCurrency(1000, undefined)
+    expect(formattedAmount).toBeNull()
+  })
 })
--- server/utils/currency.ts
+++ server/utils/currency.ts
@@ -1,7 +1,5 @@
 export const formatCurrency = (amount: number, currency: string) => {
-  if (amount === null || Number.isNaN(Number(amount)) || !currency) {
-    return null
-  }
+  if (amount === null || Number.isNaN(Number(amount)) || !currency) return null
 
   return new Intl.NumberFormat('en-GB', {
     style: 'currency',
