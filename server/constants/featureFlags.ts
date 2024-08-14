740bd98f39fd013e0f5219b5194c9cea9ccd33d4
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2024-08-14 14:03:45 +0100

    turn off feature

--- server/constants/featureFlags.ts
+++ server/constants/featureFlags.ts
@@ -33,8 +33,8 @@ export const featureFlags: FeatureFlags = {
     ],
   },
   [Features.Settings]: {
-    enabled: true,
-    allowedPrisons: ALLOW_ALL_PRISONS,
+    enabled: false,
+    allowedPrisons: [],
   },
   [Features.SocialVisitors]: {
     enabled: false,
