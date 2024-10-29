f92cd85b3b9b92eaccefea87d42836149cda3db3
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2024-10-29 12:50:40 +0000

    add comments back in

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -5,6 +5,9 @@ import helmet from 'helmet'
 export default function setUpWebSecurity(): Router {
   const router = express.Router()
 
+  // Secure code best practice - see:
+  // 1. https://expressjs.com/en/advanced/best-practice-security.html,
+  // 2. https://www.npmjs.com/package/helmet
   router.use((_req: Request, res: Response, next: NextFunction) => {
     res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
     next()
@@ -15,6 +18,12 @@ export default function setUpWebSecurity(): Router {
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
+          // This nonce allows us to use scripts with the use of the `cspNonce` local, e.g (in a Nunjucks template):
+          // <script nonce="{{ cspNonce }}">
+          // or
+          // <link href="http://example.com/" rel="stylesheet" nonce="{{ cspNonce }}">
+          // This ensures only scripts we trust are loaded, and not anything injected into the
+          // page by an attacker.
           scriptSrc: [
             "'self'",
             (_req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
