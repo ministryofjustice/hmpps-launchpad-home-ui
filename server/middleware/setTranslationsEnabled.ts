c985856a85133394cfae23723f106cba56a81cdc
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2024-11-25 11:46:26 +0000

    define translations in middleware

--- server/middleware/setTranslationsEnabled.ts
+++ server/middleware/setTranslationsEnabled.ts
@@ -5,7 +5,14 @@ import { Features } from '../constants/featureFlags'
 // eslint-disable-next-line import/prefer-default-export
 export const setTranslationsEnabled = (req: Request, res: Response, next: NextFunction) => {
   const isTranslationsEnabled = isFeatureEnabled(Features.Translations, req.user.idToken.establishment.agency_id)
+  const currentLng = req.query.lng || 'en'
+
   res.locals.isTranslationsEnabled = isTranslationsEnabled
-  res.locals.currentLng = req.query.lng || 'en'
+  res.locals.currentLng = currentLng
+  res.locals.translations = [
+    { href: '/?lng=en', lang: 'en', text: 'English' },
+    { href: '/?lng=cy', lang: 'cy', text: 'Cymraeg' },
+  ]
+
   next()
 }
