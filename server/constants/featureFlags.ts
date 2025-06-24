62532cb7b57d9c6265de4b12e5cbb0cfc6c2b158
Angela Pan <angela.pan@digital.justice.gov.uk>
2025-06-24 10:19:44 +0100

    turn off adjudications feature while we resolve INC3385328 (#367)
    * turn off adjudications feature
    
    * comment out unused code

--- server/constants/featureFlags.ts
+++ server/constants/featureFlags.ts
@@ -31,9 +31,9 @@ const COMMON_PRISON_LIST = [
   prisonAgencyIds.Woodhill,
 ]
 
-const ADJUDICATIONS_PRISON_LIST = COMMON_PRISON_LIST.filter(
+/* const ADJUDICATIONS_PRISON_LIST = COMMON_PRISON_LIST.filter(
   prison => ![prisonAgencyIds.Bullingdon, prisonAgencyIds.Chelmsford, prisonAgencyIds.Woodhill].includes(prison),
-)
+) */
 
 export const Features = {
   Adjudications: 'adjudications',
@@ -46,8 +46,8 @@ export const Features = {
 
 export const featureFlags: FeatureFlags = {
   [Features.Adjudications]: {
-    enabled: true,
-    allowedPrisons: ADJUDICATIONS_PRISON_LIST,
+    enabled: false,
+    allowedPrisons: [],
   },
   [Features.Settings]: {
     enabled: false,
