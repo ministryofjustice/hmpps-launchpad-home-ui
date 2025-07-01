aa96831f658d825684ff97b2783babcf6fc0004a
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2025-07-01 15:48:10 +0100

    add prisoner details to page error log

--- server/errorHandler.ts
+++ server/errorHandler.ts
@@ -4,14 +4,13 @@ import logger from '../logger'
 
 export default function createErrorHandler(production: boolean) {
   return (error: HTTPError, req: Request, res: Response, next: NextFunction): void => {
-    logger.error(
-      {
-        msg: `Error handling request for '${req.originalUrl}', user '${res.locals.user?.name}'`,
-        prisonerNumber: res.locals.user?.idToken?.sub,
-        agencyId: res.locals.user?.idToken?.establishment?.agency_id,
-      },
-      production ? null : error,
-    )
+    logger.error({
+      errorMsg: `Error handling request for '${req.originalUrl}'`,
+      userName: res.locals.user?.idToken?.given_name,
+      prisonerNumber: res.locals.user?.idToken?.sub,
+      agencyId: res.locals.user?.idToken?.establishment?.agency_id,
+      details: production ? null : error,
+    })
 
     res.locals.heading = 'Something went wrong'
     res.locals.subheading = 'This page could not be found.'
