76c55a738f4dbf393ebeacea0a05be92301d5cb7
chiem-nguyen-MOJ <chiem.nguyen@justice.gov.uk>
2025-09-24 09:29:30 +0100

    updating page objects

--- integration_tests/playwright/test/Features/e2e/Launchpad_e2e/launchpad_externalLinks.spec.ts
+++ integration_tests/playwright/test/Features/e2e/Launchpad_e2e/launchpad_externalLinks.spec.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import { test, expect } from '@playwright/test'
+import launchpadExternalLinksLocators from 'pages/launchpad/launchlinks'
 
 dotenv.config()
 
@@ -11,36 +12,40 @@ test.describe('Launchpad External Web Links', () => {
   })
 
   test('Assert that the user can see the content hub module', async ({ page }) => {
-    const contentHubLink = page.locator('a[href="/external/content-hub"][rel="noreferrer noopener"][target="_blank"]')
+    const contentHubLink = page.locator(launchpadExternalLinksLocators.contentHubLink)
     await contentHubLink.waitFor({ state: 'visible', timeout: 5000 })
 
-    const contentHubImg = contentHubLink.locator('img[alt="link tile image"]')
+    const contentHubImg = contentHubLink.locator(launchpadExternalLinksLocators.contentHubImg)
     await contentHubImg.waitFor({ state: 'visible', timeout: 5000 })
 
     await expect(contentHubLink).toBeVisible()
     await expect(contentHubImg).toBeVisible()
-    await expect(contentHubLink.locator('h3.govuk-heading-m')).toHaveText('Content Hub')
-    await expect(contentHubLink.locator('p.govuk-body')).toHaveText(
+    await expect(contentHubLink.locator(launchpadExternalLinksLocators.contentHubHeading)).toHaveText('Content Hub')
+    await expect(contentHubLink.locator(launchpadExternalLinksLocators.contentHubDescription)).toHaveText(
       'Watch, read and listen to local and national content.',
     )
   })
 
   test('Assert that the user can see the National Prison Radio module', async ({ page }) => {
-    const prisonRadioLink = page.locator('a[href="/external/prison-radio"][rel="noreferrer noopener"][target="_blank"]')
+    const prisonRadioLink = page.locator(launchpadExternalLinksLocators.prisonRadioLink)
     await expect(prisonRadioLink).toBeVisible()
-    await expect(prisonRadioLink.locator('img[alt="link tile image"]')).toBeVisible()
-    await expect(prisonRadioLink.locator('h3.govuk-heading-m')).toHaveText('National Prison Radio')
-    await expect(prisonRadioLink.locator('p.govuk-body')).toHaveText(
+
+    await expect(prisonRadioLink.locator(launchpadExternalLinksLocators.prisonRadioImg)).toBeVisible()
+    await expect(prisonRadioLink.locator(launchpadExternalLinksLocators.prisonRadioHeading)).toHaveText(
+      'National Prison Radio',
+    )
+    await expect(prisonRadioLink.locator(launchpadExternalLinksLocators.prisonRadioDescription)).toHaveText(
       'Listen to 24/7 music, talk, requests and playback',
     )
   })
 
   test('Assert that the user can see the inside time module', async ({ page }) => {
-    const insideTimeLink = page.locator('a[href="/external/inside-time"][rel="noreferrer noopener"][target="_blank"]')
+    const insideTimeLink = page.locator(launchpadExternalLinksLocators.insideTimeLink)
     await expect(insideTimeLink).toBeVisible()
-    await expect(insideTimeLink.locator('img[alt="link tile image"]')).toBeVisible()
-    await expect(insideTimeLink.locator('h3.govuk-heading-m')).toHaveText('Inside Time')
-    await expect(insideTimeLink.locator('p.govuk-body')).toHaveText(
+
+    await expect(insideTimeLink.locator(launchpadExternalLinksLocators.insideTimeImg)).toBeVisible()
+    await expect(insideTimeLink.locator(launchpadExternalLinksLocators.insideTimeHeading)).toHaveText('Inside Time')
+    await expect(insideTimeLink.locator(launchpadExternalLinksLocators.insideTimeDescription)).toHaveText(
       'Read the national newspaper for prisoners and detainees.',
     )
   })
