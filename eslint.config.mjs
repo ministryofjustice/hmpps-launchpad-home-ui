c5f184e61290e9ca2c9863dd81d89afcd32685ea
chiem-nguyen-MOJ <chiem.nguyen@justice.gov.uk>
2025-09-30 23:38:44 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
-export default hmppsConfig({ extraIgnorePaths: ['assets/**/*.js'] })
+export default [
+  ...hmppsConfig({ extraIgnorePaths: ['assets/**/*.js'] }),
+  {
+    files: ['integration_tests/support/playwright.global-setup.js'],
+    rules: {
+      'import/no-unresolved': 'off',
+    },
+  },
+]
