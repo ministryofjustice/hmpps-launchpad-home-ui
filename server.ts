cf7b346f7fbbb828c526c400ca503c35b15f54d6
chiem-nguyen-MOJ <chiem.nguyen@justice.gov.uk>
2025-10-02 14:37:51 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,13 +1,15 @@
-// Require app insights before anything else to allow for instrumentation of bunyan and express
 import 'applicationinsights'
 
 import { app, metricsApp } from './server/index'
 import logger from './logger'
 
-app.listen(app.get('port'), '0.0.0.0', () => {
-  logger.info(`Server listening on port ${app.get('port')} (bound to 0.0.0.0)`)
+const PORT = app.get('port')
+const HOST = process.env.HOST || '0.0.0.0'
+
+app.listen(PORT, HOST, () => {
+  logger.info(`Server listening on http://${HOST}:${PORT}`)
 })
 
-metricsApp.listen(metricsApp.get('port'), '0.0.0.0', () => {
-  logger.info(`Metrics server listening on port ${metricsApp.get('port')} (bound to 0.0.0.0)`)
+metricsApp.listen(metricsApp.get('port'), () => {
+  logger.info(`Metrics server listening on port ${metricsApp.get('port')}`)
 })
