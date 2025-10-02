cae1c5426d535d7f3d964b2819bbfadd7d39666a
chiem-nguyen-MOJ <chiem.nguyen@justice.gov.uk>
2025-10-02 14:47:22 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -4,12 +4,15 @@ import { app, metricsApp } from './server/index'
 import logger from './logger'
 
 const PORT = app.get('port')
+const METRICS_PORT = metricsApp.get('port')
 const HOST = process.env.HOST || '0.0.0.0'
 
+// Main  app binds to 0.0.0.0
 app.listen(PORT, HOST, () => {
   logger.info(`Server listening on http://${HOST}:${PORT}`)
 })
 
-metricsApp.listen(metricsApp.get('port'), () => {
-  logger.info(`Metrics server listening on port ${metricsApp.get('port')}`)
+// Metrics app also binds to 0.0.0.0
+metricsApp.listen(METRICS_PORT, HOST, () => {
+  logger.info(`Metrics server listening on http://${HOST}:${METRICS_PORT}`)
 })
