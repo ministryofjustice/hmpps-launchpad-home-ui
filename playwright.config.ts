920e3b0c0d1b1c24ec242422ec42110d76bb02d1
chiem-nguyen-MOJ <chiem.nguyen@justice.gov.uk>
2025-10-14 09:17:42 +0100

    retry config for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     video: 'retain-on-failure',
   },
   timeout: 30000,
-  retries: process.env.CI ? 3 : 2, // More retries in CI
+  retries: process.env.CI ? 1 : 1, // More retries in CI
   reporter: [
     ['html', { open: 'never', outputFolder: 'playwright-report' }],
     ['list'],
