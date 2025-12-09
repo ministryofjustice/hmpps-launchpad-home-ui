cb31f6fd82dcf2051701b4d22ab3eec6a70a4912
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2025-12-09 12:28:31 +0000

    remove circleci configs and update readme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,5 @@ import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
 export default hmppsConfig({
   extraIgnorePaths: ['assets/**/*.js', 'test_results/**', 'test-results/**', 'playwright-report/**'],
+  extraPathsAllowingDevDependencies: ['.allowed-scripts.mjs'],
 })
