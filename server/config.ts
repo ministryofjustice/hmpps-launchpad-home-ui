49f28c7708909e3852ecf1a2bf17794d9313c85e
angela-pan-moj <angela.pan@digital.justice.gov.uk>
2026-04-08 12:25:09 +0100

    turning on think through nutrition tile for Styal, New Hall, Berwyn and Wayland

--- server/config.ts
+++ server/config.ts
@@ -182,7 +182,7 @@ export default {
       prisonerContentHubURL: 'https://berwyn.content-hub.prisoner.service.justice.gov.uk',
       selfServiceURL: 'https://bwiclient.unilink.prisoner.service.justice.gov.uk:1108',
       hideHomepageEventsSummaryAndProfileLinkTile: true,
-      hideThinkThroughNutrition: true,
+      hideThinkThroughNutrition: false,
     },
     {
       agencyId: 'CDI',
@@ -242,7 +242,7 @@ export default {
       agencyId: 'NHI',
       prisonerContentHubURL: 'https://newhall.content-hub.prisoner.service.justice.gov.uk',
       selfServiceURL: 'https://nhiclient.unilink.prisoner.service.justice.gov.uk:96',
-      hideThinkThroughNutrition: true,
+      hideThinkThroughNutrition: false,
     },
     {
       agencyId: 'RNI',
@@ -266,7 +266,7 @@ export default {
       agencyId: 'STI',
       prisonerContentHubURL: 'https://styal.content-hub.prisoner.service.justice.gov.uk',
       selfServiceURL: 'https://sticlient.unilink.prisoner.service.justice.gov.uk:1102',
-      hideThinkThroughNutrition: true,
+      hideThinkThroughNutrition: false,
     },
     {
       agencyId: 'WHI',
@@ -278,7 +278,7 @@ export default {
       agencyId: 'WLI',
       prisonerContentHubURL: 'https://wayland.content-hub.prisoner.service.justice.gov.uk',
       selfServiceURL: 'https://wliclient.unilink.prisoner.service.justice.gov.uk:1114',
-      hideThinkThroughNutrition: true,
+      hideThinkThroughNutrition: false,
     },
     {
       agencyId: 'WNI',
