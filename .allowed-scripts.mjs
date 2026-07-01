00e970f9589da29e62a3272ed0557f9be8a9eac4
Elliot Ward <elliot.ward@digital.justice.gov.uk>
2026-07-01 13:43:47 +0100

    LNP-1719: ⬆️   update allowed scripts.

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,11 +3,11 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // Needed for watching files during development
-    'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
+    'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     // CPU profiler and native binaries used by @sentry
-    'node_modules/@sentry-internal/node-cpu-profiler@2.2.0': 'ALLOW',
+    'node_modules/@sentry-internal/node-cpu-profiler@2.4.1': 'ALLOW',
     // Needed by Cypress for pulling in the Cypress binary
-    'node_modules/cypress@15.11.0': 'ALLOW',
+    'node_modules/cypress@15.18.0': 'ALLOW',
     // Provides native integration, supporting the ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // Needed by jest for running tests in watch mode
@@ -15,6 +15,6 @@ export default configureAllowedScripts({
     // Needed by playwright for detecting file system changes during test runs
     'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
-    'node_modules/unrs-resolver@1.9.0': 'ALLOW',
+    'node_modules/unrs-resolver@1.12.2': 'ALLOW',
   },
 })
