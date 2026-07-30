9e7d839e2abc70c15dd63ad68310bab74713908b
Ryan Kendall <1352756+ryantk@users.noreply.github.com>
2026-07-30 11:40:09 +0100

    links test overhaul

--- server/services/links/index.test.ts
+++ server/services/links/index.test.ts
@@ -1,8 +1,19 @@
-import { LaunchpadUser } from '@ministryofjustice/hmpps-prisoner-auth'
-import LinkService from '.'
-import { ifWithinActiveAgency } from './activeAgencies'
+import {
+  expectToShowEveryTime,
+  expectToShowWhenEstablishmentValueIsSetOnly,
+  expectToShowWhenUserAllowListedOnly,
+  expectToShowWhenUserWithinActiveAgencyOnly,
+} from './index.test.helpers'
 
 jest.mock('./activeAgencies')
+
+jest.mock('../../utils/utils', () => ({
+  ...jest.requireActual('../../utils/utils'),
+  getEstablishmentData: jest.fn().mockReturnValue({
+    hideInsideTime: true,
+    hideThinkThroughNutrition: true,
+  }),
+}))
 
 jest.mock('../../config', () => ({
   ...jest.requireActual('../../config').default,
@@ -10,79 +21,34 @@ jest.mock('../../config', () => ({
 }))
 
 describe('LinkService', () => {
-  let linkService: LinkService
-
-  const activeAgenciesMap: Record<string, string[]> = {
-    [process.env.MANAGE_APPS_UI_URL]: ['RNI'],
-    [process.env.PIN_PHONES_UI_URL]: ['RNI'],
-  }
-
-  beforeEach(() => {
-    linkService = new LinkService()
-
-    ;(ifWithinActiveAgency as jest.Mock).mockImplementation(async (agencyId: string, serviceUrl: string) => {
-      return activeAgenciesMap[serviceUrl].includes(agencyId)
+  describe('getHomepageLinks', () => {
+    describe('Manage Apps tile', () => {
+      expectToShowWhenUserWithinActiveAgencyOnly(0)
+      expectToShowWhenUserAllowListedOnly(0)
     })
-  })
 
-  it('hides think through nutrition for certain establishments', async () => {
-    const { links } = await linkService.getHomepageLinks(
-      { idToken: { establishment: { agency_id: 'BFI' }, sub: 'prisoner 1' } } as LaunchpadUser,
-      'en',
-    )
-    const thinkThroughNutritionTile = links[5]
+    describe('Self Service tile', () => {
+      expectToShowEveryTime(1)
+    })
 
-    expect(thinkThroughNutritionTile.show).toBe(false)
-  })
+    describe('Content-hub tile', () => {
+      expectToShowEveryTime(2)
+    })
 
-  test.each(['prisoner 1', 'prisoner 2', 'prisoner 3'])(
-    'displays manage app link for allowed users in Ranby',
-    async prisonerId => {
-      const { links } = await linkService.getHomepageLinks(
-        { idToken: { establishment: { agency_id: 'RNI' }, sub: prisonerId } } as LaunchpadUser,
-        'en',
-      )
+    describe('National Prison Radio tile', () => {
+      expectToShowEveryTime(3)
+    })
 
-      const manageAppsTile = links[0]
-      const pinPhoneTile = links[6]
+    describe('Inside Time tile', () => {
+      expectToShowWhenEstablishmentValueIsSetOnly(4, 'hideInsideTime', false, true)
+    })
 
-      expect(manageAppsTile.show).toBe(true)
-      expect(pinPhoneTile.show).toBe(true)
-    },
-  )
+    describe('Think Through Nutrition tile', () => {
+      expectToShowWhenEstablishmentValueIsSetOnly(5, 'hideThinkThroughNutrition', false, true)
+    })
 
-  it('does not display manage app link for disallowed users', async () => {
-    const { links } = await linkService.getHomepageLinks(
-      { idToken: { establishment: { agency_id: 'RNI' }, sub: 'prisoner 4' } } as LaunchpadUser,
-      'en',
-    )
-
-    const manageAppsTile = links[0]
-
-    expect(manageAppsTile.show).toBe(false)
-  })
-
-  it('does not display manage app link for disallowed users even if available at that prison', async () => {
-    const { links } = await linkService.getHomepageLinks(
-      { idToken: { establishment: { agency_id: 'RNI' }, sub: 'prisoner 4' } } as LaunchpadUser,
-      'en',
-    )
-
-    const manageAppsTile = links[0]
-
-    expect(manageAppsTile.show).toBe(false)
-  })
-
-  it('does not display manage app link locations other than Ranby', async () => {
-    const { links } = await linkService.getHomepageLinks(
-      { idToken: { establishment: { agency_id: 'BNI' }, sub: 'prisoner 1' } } as LaunchpadUser,
-      'en',
-    )
-
-    const manageAppsTile = links[0]
-    const pinPhoneTile = links[6]
-
-    expect(manageAppsTile.show).toBe(false)
-    expect(pinPhoneTile.show).toBe(false)
+    describe('PIN Phone tile', () => {
+      expectToShowWhenUserWithinActiveAgencyOnly(6)
+    })
   })
 })
