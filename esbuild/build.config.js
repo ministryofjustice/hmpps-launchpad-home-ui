692d408b30a72e042e3899520cbd57b3b291f91c
Ryan Kendall <1352756+ryantk@users.noreply.github.com>
2026-08-21 13:57:54 +0100

    fix assets and localisation

--- esbuild/build.config.js
+++ esbuild/build.config.js
@@ -24,6 +24,11 @@ const getBuildConfig = () => {
           to: path.join(cwd, 'dist/server/views'),
           watch: isWatchMode,
         },
+        {
+          from: path.join(cwd, 'server/locales/**/*'),
+          to: path.join(cwd, 'dist/server/locales'),
+          watch: isWatchMode,
+        },
       ],
     },
 
